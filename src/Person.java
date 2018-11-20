22d0f8b8ab5721ed5a9aa7a12e60084cc467498d
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-11-20 11:25:44 -0600

    refactored code based on walkthrough solutions

--- src/Person.java
+++ src/Person.java
@@ -14,7 +14,4 @@ public class Person {
         return String.format("%s %s", firstName, lastName);
     }
 
-    public String sortName() {
-        return String.format("%s, %s", lastName, firstName);
-    }
 }
