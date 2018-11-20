50e5d68cd2e5826bb6ad29191ee7640ff8b922ab
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-11-19 19:11:13 -0600

    fixed warnings

--- src/Author.java
+++ src/Author.java
@@ -3,7 +3,7 @@
  */
 import java.util.ArrayList;
 import java.util.List;
-
+@SuppressWarnings("unchecked")
 public class Author extends Person {
     private List books;
 
@@ -15,6 +15,7 @@ public class Author extends Person {
     /**
      * @deprecated Use publishedBooks instead
      */
+
     @Deprecated
     public List<String> getBooks() {
         return books;
